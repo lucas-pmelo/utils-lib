44e5782fca67ea5641771a508b5de673c92a2e21
Lucas Melo <lucas.pinheiro.melo@gmail.com>
2026-04-02 23:05:19 -0300

    feat: adjust withErrorHandler

--- packages/handlers/src/elysia-handler.ts
+++ packages/handlers/src/elysia-handler.ts
@@ -1,3 +1,5 @@
+import ClbError from "./errors/clb-error";
+
 export function createResponse<T>({ status, data }: { status: number; data: T }): Response {
   return new Response(JSON.stringify(data), {
     status,
@@ -7,11 +9,15 @@ export function createResponse<T>({ status, data }: { status: number; data: T })
 
 export async function withErrorHandler<T>(
   fn: () => Promise<T>,
-  errorMessage: string,
+  message: string,
 ): Promise<T | Response> {
   try {
     return await fn();
-  } catch {
-    return createResponse({ status: 500, data: { error: errorMessage } });
+  } catch (error) {
+    if (error && typeof error === "object" && (error as ClbError).isTreated) {
+      const clbError = error as ClbError;
+      return createResponse({ status: clbError.statusCode, data: clbError.toObject() });
+    }
+    return createResponse({ status: 500, data: { error: message } });
   }
 }
